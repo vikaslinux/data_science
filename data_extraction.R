6ab39ac20e3177f25c55bf6e105259f23db66f9e
prakhardogra921 <prakhardogra@icloud.com>
2015-02-14 22:21:43 +0530

    Update data_extraction.R

--- data_extraction.R
+++ data_extraction.R
@@ -1,13 +1,20 @@
+library("RCurl")
+library("ROAuth")
+library("OAuth")
 library("twitteR")
+
 requestURL <-  "https://api.twitter.com/oauth/request_token"
 accessURL =    "https://api.twitter.com/oauth/access_token"
 authURL =      "https://api.twitter.com/oauth/authorize"
-consumerKey =   "PqSPfWBPGNbnh7ONUDddsxZVW"
-consumerSecret = "xigDFiLAOkzsrlbXempi1KDjbYlIR09TCTjjmjpkEsF702fePR"
+
+consumerKey =   "-----------------"   #type yor own key
+consumerSecret = "--------------------------"    #type your own secret
+
 twitCred <- OAuthFactory$new(consumerKey=consumerKey,consumerSecret=consumerSecret,requestURL=requestURL,accessURL=accessURL,authURL=authURL);
 twitCred$handshake(cainfo="cacert.pem")
-7931394
+
+#run the below commands after getting the authorization code
+
 registerTwitterOAuth(twitCred)
-me <- getUser("arvindkejrival",cainfo="cacert.pem")
+me <- getUser("arvindkejrival",cainfo="cacert.pem")    #type whatever twitter username you want
 wea_tweets = userTimeline("arvindkejrival",cainfo="cacert.pem")
-s
