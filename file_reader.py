94e1615468a0f1677dea487010d5be5289f3ca05
vikaslinux <vikasm.dtu@gmail.com>
2015-03-04 13:46:01 +0530

    Added the functions for removing RT

--- file_reader.py
+++ file_reader.py
@@ -1,11 +1,11 @@
-import csv,unicodedata
-from unidecode import *
-with open('arvindkejriwal_tweets.csv', newline='',encoding='utf-8') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+import csv,sys
+file=sys.argv[1]
+tweets=[]
+with open(file, newline='\n',encoding='utf-8') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     print(type(spamreader))
     for value in spamreader:
-        for string in value:
-            if type(string) == str:
-                print (string)
-            else:
-                print (string.decode("utf-8"))
+           tweets.append(value[2])
+           print(value[2])
+
